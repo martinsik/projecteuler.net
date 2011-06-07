88671f5012149138bdc1f28d72ce50261df4b838
Martin Sikora <martin.sikora@symbio.cz>
2011-06-07 14:18:14 +0200

    problem11

--- problem-11/src/com/martinsikora/Problem11.java
+++ problem-11/src/com/martinsikora/Problem11.java
@@ -43,7 +43,6 @@ public class Problem11 {
 				product = grid[j][k] * grid[j+1][k] * grid[j+2][k] * grid[j+3][k];
 				if (product > max) {
 					max = product;
-					point[0] = j; point[1] = k;
 				}
 			}
 		}
@@ -57,7 +56,6 @@ public class Problem11 {
 				product = grid[j][k] * grid[j][k+1] * grid[j][k+2] * grid[j][k+3];
 				if (product > max) {
 					max = product;
-					point[0] = j; point[1] = k;
 				}
 			}
 		}
@@ -65,14 +63,23 @@ public class Problem11 {
 		/**
 		 * diagonal
 		 */
+		int product2, product3, product4, product5, product6, product7, product8;
 		for (int j = 0; j <= grid.length - 4; j++) {
 			for (int k = 0; k <= grid[j].length - 4; k++) {
-				cycles++;
-				product = grid[j][k] * grid[j+1][k+1] * grid[j+2][k+2] * grid[j+3][k+3];
-				if (product > max) {
-					max = product;
-					point[0] = j; point[1] = k;
-				}
+				cycles += 4;
+				product = grid[j][k] * grid[j+1][k+1] * grid[j+2][k+2] * grid[j+3][k+3]; // top-left
+				//product5 = grid[j][k] * grid[j-1][k+1] * grid[j-2][k+2] * grid[j-3][k+3];
+				
+				product4 = grid[j][19-k-3] * grid[j+1][19-k-2] * grid[j+2][19-k-1] * grid[j+3][19-k]; // top-right
+				
+				product2 = grid[19-j-3][k] * grid[19-j-2][k+1] * grid[19-j-1][k+2] * grid[19-j][k+3]; // bottom-left
+				product3 = grid[19-j-3][19-k-3] * grid[19-j-2][19-k-2] * grid[19-j-1][19-k-1] * grid[19-j][19-k]; // bottom-right
+				
+				
+				//product6 = grid[j][k] * grid[j-1][k+1] * grid[j-2][k+2] * grid[j-3][k+3];
+				
+				max = Math.max(max, Math.max(Math.max(product, product2), Math.max(product3, product4)));
+				//max = Math.max(max, product5);
 			}
 		}
 		
