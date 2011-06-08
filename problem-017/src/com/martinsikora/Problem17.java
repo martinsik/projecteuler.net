1a17a03b9c5706d717d559683bb00838ace8a412
martinsik <martin.sikora@symbio.cz>
2011-06-08 21:46:19 +0200

    problem 17

---
+++ problem-017/src/com/martinsikora/Problem17.java
@@ -0,0 +1,45 @@
+package com.martinsikora;
+
+public class Problem17 {
+
+	static int[] numbers = {4,3,3,5,4,4,3,5,5,4};
+	
+	/*
+	 * 10 - ten
+	 * 11 - eleven
+	 * 12 - twelve
+	 * 13 - thirteen
+	 * 14 - fourteen
+	 * 15 - fifteen
+	 * 16 - sixteen
+	 * 17 - seventeen
+	 * 18 - eighteen
+	 * 19 - nineteen
+	 */
+	static int[] numbersTeens = {3,6,6,8,8,7,7,9,8,8};
+	
+	/*
+	 * 20 - twenty
+	 * 30 - thirty
+	 * 40 - forty
+	 * 50 - fifty
+	 * 60 - sixty
+	 * 70 - seventy
+	 * 80 - eighty
+	 * 90 - ninety
+	 */
+	static int[] tens = {6,6,5,5,5,7,6,6};
+	
+	static int hundred = 7;
+	
+	static int thousand = 8; 
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
